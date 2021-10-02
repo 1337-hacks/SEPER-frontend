19a546e20b389c02d7aa4b95afe16b8d2032093b
1337-hacks <elijahnucum.exe@gmail.com>
2021-10-02 21:44:06 +1300

    c002/Fixed Data Fetch

--- src/Components/Pages/DisplayEvidence/index.jsx
+++ src/Components/Pages/DisplayEvidence/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 import { Link } from "react-router-dom";
@@ -10,40 +10,48 @@ import {
   PageHeader
 } from "./DisplayEvidenceStyle";
 
-import articles from "./../../Evidence-Table/articles";
 import Table from "../../Evidence-Table/evidencetable";
 import tablecolumns from "../../Evidence-Table/tablecolumns";
 import Dropdown from "../../Evidence-Table/dropdown";
-import Styles from "../../Evidence-Table/tablestyle";
+//import Styles from "../../Evidence-Table/tablestyle";
 
-const DisplayEvidencePage = () => {
+const DisplayEvidencePage = function() {
 
-  /* Logic goes here eg. pull data from DB */
-  
+  /* Fetch data from Backend */
+
+  const [articles, setArticles] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("./routes/articles")
+			.then((articles) => setArticles(articles.data))
+			.catch((err) => console.log(err));
+	}, []);
+
   return (
-      <PageWrapper>
-        {/* Container for Page Background */}
-        <PageContainer>
-          {/* Container for Page Content */}
-          <PageHeader>View Evidence</PageHeader>
-          <Data>
-            <div>
-                <h2>Select SE Practice to get evidence for the claimed benefits</h2>
-                <Dropdown>
-                  <p>Select SE Practice: </p>
-                </Dropdown>
-                {/* <Styles> */}
-                  <Table data={articles} columns={tablecolumns}/>
-                {/* </Styles> */}
-            </div>
-              <Link to="/">
-            <DisplayButton> Home </DisplayButton>
-          </Link>
-          </Data>
-          
-        </PageContainer>
-      </PageWrapper>
-    );
+    <PageWrapper>
+      {/* Container for Page Background */}
+      <PageContainer>
+        {/* Container for Page Content */}
+        <PageHeader>View Evidence</PageHeader>
+        <Data>
+          <div>
+              <h2>Select SE Practice to get evidence for the claimed benefits</h2>
+              <Dropdown>
+                <p>Select SE Practice: </p>
+              </Dropdown>
+              {/* <Styles> */}
+                <Table data={articles} columns={tablecolumns}/>
+              {/* </Styles> */}
+          </div>
+            <Link to="/">
+          <DisplayButton> Home </DisplayButton>
+        </Link>
+        </Data>
+        
+      </PageContainer>
+    </PageWrapper>
+  );
 };
 
 export default DisplayEvidencePage;
